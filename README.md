9c53128332f5667973b912dfb3459cca20f3fc74
Lily <diesel1840@gmail.com>
2026-01-02 05:24:00 +0000

    Added simple ANSI colors

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 A little library written in C for my general use
 
-The name paddle stems from both my nom de clavier: lily*pads*, and the tool which aids in navigating the C ;3
+The name paddle stems from both my nom de clavier: lily*pads*, and the tool which aids in navigating the high C's ;3
+
+### TODO
+
+- [ ] Add stack repo
+- [x] Ansi Colors
---
+++ src/colors.h
@@ -0,0 +1,17 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+// Color codes obtained from https://gist.github.com/RabaDabaDoba/145049536f815903c79944599c6f952a
+
+#define BLK "\e[0;30m"
+#define RED "\e[0;31m"
+#define GRN "\e[0;32m"
+#define YEL "\e[0;33m"
+#define BLU "\e[0;34m"
+#define MAG "\e[0;35m"
+#define CYN "\e[0;36m"
+#define WHT "\e[0;37m"
+
+#define RESET "\e[0m"
+
+#endif
